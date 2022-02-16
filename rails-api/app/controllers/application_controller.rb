d61cc3a3062f98d308535e70b73930493449eb98
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-02-16 14:17:20 +0900

    サインアップ　サインイン機能実装のためapplication_controller.rb編集

--- rails-api/app/controllers/application_controller.rb
+++ rails-api/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::Helpers
+
+  helper_method :current_user, :user_signed_in?
+  #skip_before_action :verify_authenticity_token
 end
