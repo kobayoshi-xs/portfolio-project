f0533b48998ca2b7070b4bf6f440cff7f8cc3542
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-03-11 17:11:11 +0900

    共通のヘッダーコンポーネント./front/react-app/src/components/layouts/Header.tsx作成

---
+++ front/react-app/src/components/layouts/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { Container, Grid } from "@mui/material"
+import { styled } from "@mui/material/styles"
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+
+const HeaderButton = styled(Button)({
+    backgroundColor: 'red'
+});
+
+const CommonHeader: React.VFC = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > *': {
+          m: 1,
+        },
+      }}
+    >
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+        <HeaderButton>One</HeaderButton>
+        <HeaderButton>Two</HeaderButton>
+        <HeaderButton>Three</HeaderButton>
+      </ButtonGroup>
+      <ButtonGroup variant="text" aria-label="text button group">
+        <HeaderButton>One</HeaderButton>
+        <HeaderButton>Two</HeaderButton>
+        <HeaderButton>Three</HeaderButton>
+      </ButtonGroup>
+    </Box>
+  )
+}
+
+export default CommonHeader
