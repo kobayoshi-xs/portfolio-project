3a8f603c08b6d26107e5aaf6316b9b4ebdb5758d
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-04-01 17:33:27 +0900

    ./front/react-app/src/components/pages/Sign.up.tsxに新規作成画面作成

--- front/react-app/src/App.tsx
+++ front/react-app/src/App.tsx
@@ -3,13 +3,15 @@ import React, { useState, useEffect, createContext } from "react";
 
 /*import Home from "components/pages/Home"*/
 import CommonLayout from 'components/layouts/CommonLayout';
+import SignUp from 'components/pages/SignUp';
 
 import { getCurrentUser } from "lib/api/auth"
 import { User } from "interfaces/index"
 
 const App: React.FC = () => {
   return (
-    <CommonLayout />
+    /*<CommonLayout />*/
+      <SignUp />
   )
 }
 
--- front/react-app/src/components/pages/SignUp.tsx
+++ front/react-app/src/components/pages/SignUp.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import Cookies from "js-cookie"
 
 import Box from '@mui/material/Box';
@@ -11,39 +12,96 @@ import TextField from '@mui/material/TextField';
 
 import { signUp } from "lib/api/auth"
 import { SignUpData } from "interfaces"
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+import { signIn } from "lib/api/auth"
+import { SignInData } from "interfaces";
 
 const SignUp: React.VFC = () => {
+  const [name, setName] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("")
+
+  const { register, handleSubmit, control } = useForm<SignUpData>();
+  const onSubmit: SubmitHandler<SignUpData> = (data) => console.log(data);
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Card sx={{ minWidth: 275 }}>
           <CardContent>
             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Word of the Day
+              <Controller
+                name="name"
+                control={control}
+                /*defaultValue=""*/
+                rules={{
+                  required: "入力してください",
+                  minLength: { value: 8, message: "8文字以上にしてください" },
+                  maxLength: { value: 20, message: "20文字以内にしてください" }
+                }}
+                render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="名前"
+                  onChange={event => setName(event.target.value)}
+                />
+                )}
+              />
             </Typography>
-            <Typography variant="h5" component="div">
-              be{bull}nev{bull}o{bull}lent
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              <Controller
+                name="email"
+                control={control}
+                /*defaultValue={email}*/
+                rules={{
+                  required: "入力してください",
+                }}
+                render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="メールアドレス"
+                  onChange={event => setEmail(event.target.value)}
+                />
+                )}
+              />
             </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              adjective
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              <Controller
+                name="password"
+                control={control}
+                /*defaultValue={password}*/
+                rules={{
+                  required: "入力してください"
+                }}
+                render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="パスワード"
+                   onChange={event => setPassword(event.target.value)}
+                />
+                )}
+              />
             </Typography>
-            <Typography variant="body2">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              <Controller
+                name="passwordConfirmation"
+                control={control}
+                /*defaultValue={passwordConfirmation}*/
+                rules={{
+                  required: "入力してください"
+                }}
+                render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="パスワード（確認用）"
+                  onChange={event => setPasswordConfirmation(event.target.value)}
+                />
+                )}
+              />
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Learn More</Button>
+            <Button size="small">新規登録</Button>
           </CardActions>
         </Card>
       </form>
