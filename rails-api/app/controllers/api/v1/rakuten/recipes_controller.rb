d81f40ea60e3d542727c1ff008dba20a86ff71c8
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-05-05 21:46:59 +0900

    rails-api recipes_controllerにRakutenWebService::Recipeを追加

--- rails-api/app/controllers/api/v1/rakuten/recipes_controller.rb
+++ rails-api/app/controllers/api/v1/rakuten/recipes_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::Rakuten::RecipesController < ApplicationController
   def search
+    if params[:keyword]
+      @recips = RakutenWebService::Recipe.search(keyword: params[:keyword])
+    end
   end
 end
