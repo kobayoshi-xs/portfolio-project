17db711b132deb4f9f5161107a78c24b6343d686
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-05-06 17:10:58 +0900

    rails-api recipesコントローラーno

--- rails-api/app/controllers/api/v1/rakuten/recipes_controller.rb
+++ rails-api/app/controllers/api/v1/rakuten/recipes_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Rakuten::RecipesController < ApplicationController
   def search
     if params[:keyword]
       @recips = RakutenWebService::Recipe.search(keyword: params[:keyword])
+      render json: { status: 200, data: @recips }
     end
   end
 end
