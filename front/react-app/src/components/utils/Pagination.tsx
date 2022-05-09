5af5f92004e95adeebb54563a6f290d3cc2279be
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-05-09 17:30:45 +0900

    front utils Pagination.tsx軽微な修正

--- front/react-app/src/components/utils/Pagination.tsx
+++ front/react-app/src/components/utils/Pagination.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-export default function PaginationOutlined() {
+export const PaginationOutlined: React.VFC = () => {
   return (
     <Stack spacing={2}>
       <Pagination count={10} variant="outlined" />
