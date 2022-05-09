f17394d93e563905a6fee3a76fa0f3c5cc9fc8ea
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-05-09 17:16:50 +0900

    front RecipeArea.tsxに検索結果表示エリア実装のため、MUIのpaperを追加

--- front/react-app/src/components/layouts/RecipeArea.tsx
+++ front/react-app/src/components/layouts/RecipeArea.tsx
@@ -3,13 +3,32 @@ import React from "react";
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+
+import SearchBar from "components/utils/SearchBar";
 
 export const RecipeArea: React.VFC = () => {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      <Container fixed>
+        {/*<Box sx={{ bgcolor: '#f0f4c3', height: '200vh' }} />*/}
+        <div>
+          <SearchBar />
+        </div>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: 1,
+              width: 100,
+              height: 300,
+            },
+          }}
+        >
+          <Paper elevation={3} />
+        </Box>
       </Container>
     </React.Fragment>
   );
