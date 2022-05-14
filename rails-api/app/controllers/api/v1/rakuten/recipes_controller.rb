5548c86d203ba6ed8ad0422d81a68127a956ee04
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-05-14 22:10:01 +0900

    rails-api controllers rakuten recipesにlarge_categories追加

--- rails-api/app/controllers/api/v1/rakuten/recipes_controller.rb
+++ rails-api/app/controllers/api/v1/rakuten/recipes_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::Rakuten::RecipesController < ApplicationController
   def search
-    if params[:keyword]
-      @recips = RakutenWebService::Recipe.search(keyword: params[:keyword])
-      render json: { status: 200, data: @recips }
-    end
+    #if params[:keyword]
+      #@recips = RakutenWebService::Recipe.search(keyword: params[:keyword])
+      #render json: { status: 200, data: @recips }
+    #end
+    @recipes = RakutenWebService::Recipe.large_categories
   end
 end
