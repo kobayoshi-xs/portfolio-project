5f6b26e86c1e545e3d80c4f518c015c38954fb1c
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-06-28 07:11:19 +0900

    front components/pagesにレシピランキングを元にデータを表示するファイルを新規作成

---
+++ front/react-app/src/components/pages/RecipeList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const RecipeList: React.VFC = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default RecipeList
--- front/react-app/src/components/utils/SideMenu.tsx
+++ front/react-app/src/components/utils/SideMenu.tsx
@@ -43,16 +43,6 @@ const SideMenu: React.VFC = () => {
             </ListItem>
           ))}
         </List>
-        {/*<List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-          </List>*/}
       </Box>
     </Drawer>
   )
