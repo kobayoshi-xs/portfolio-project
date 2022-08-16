80718d21212c37b9903c535f20656dd43b098cad
yoshi <yoshi@yoshinoMacBook-Pro.local>
2022-08-16 16:37:30 +0900

    react UnderMenu.tsxにMUIリスト追加

--- front/react-app/src/components/pages/UnderMenu.tsx
+++ front/react-app/src/components/pages/UnderMenu.tsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+import axios from "axios";
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 60,
+    lineHeight: '60px',
+  }));
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
+const lightTheme = createTheme({ palette: { mode: 'light' } });
 
 const UnderMenu: React.VFC = () => {
+  const [APIDatas, setAPIDatas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/v1/rakuten/lists').then((response) => {
+      setAPIDatas(response.data);
+    });
+  }, []);
+  console.log(APIDatas)
+
   return(
-    <div></div>
-  )
+    <Grid container spacing={2}>
+      {[lightTheme, darkTheme].map((theme, index) => (
+        <Grid item xs={6} key={index}>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                p: 2,
+                bgcolor: 'background.default',
+                display: 'grid',
+                gridTemplateColumns: { md: '1fr 1fr' },
+                gap: 2,
+              }}
+            >
+              {[0, 1, 2, 3, 4, 6, 8, 12, 16, 24].map((elevation) => (
+                <Item key={elevation} elevation={elevation}>
+                  {`elevation=${elevation}`}
+                </Item>
+              ))}
+            </Box>
+          </ThemeProvider>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 export default UnderMenu
